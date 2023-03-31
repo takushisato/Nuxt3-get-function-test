f325757d8129a37a96ba7bac567a3c58cd14b815
taku820801 <takuchanpower@gmail.com>
2023-03-31 22:51:01 +0900

    API取得のサンプル関数などを追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>テストだよ</h1>
+<!--  console.logで確認する目的のためテンプレート側は適当-->
+  <h1>テスト</h1>
 </template>
 <script setup>
-  import {hoge} from "~/composables/testStore";
+  // インポートなくても動く
+  import {getHoantyoData, hoge} from "~/composables/testStore";
 
-  const get = hoge()
-  console.log(get)
+  // 問題なし
+  const getSample = hoge()
+  console.log(getSample)
+
+  // API取得問題なし
+  const umiyoho = getHoantyoData()
+  console.log(umiyoho)
+
+  // await付けても問題なく動く
+  const umiyoho2 = await getHoantyoData()
+  console.log(umiyoho2)
 </script>
