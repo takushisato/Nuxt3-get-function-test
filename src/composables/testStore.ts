bf20bd42a1b5752e374c57ccce2c1b8d6fbb2685
taku820801 <takuchanpower@gmail.com>
2023-03-31 23:30:15 +0900

    prettierインストール

--- src/composables/testStore.ts
+++ src/composables/testStore.ts
@@ -7,9 +7,9 @@ export const hoge = () => {
 // 試し関数その２
 // ここではサンプルとして海上保安庁が無料公開しているAPIを取得している（岩手県の海上データを取得しています）
 export async function getHoantyoData() {
-  const { data, error } = await useFetch('https://tide736.net/api/get_tide.php/?pc=3&hc=7&yr=2023&mn=3&dy=31&rg=day', {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  });
-  return data
+    const {data, error} = await useFetch('https://tide736.net/api/get_tide.php/?pc=3&hc=7&yr=2023&mn=3&dy=31&rg=day', {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    });
+    return data
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,20 @@
 <template>
-<!--  console.logで確認する目的のためテンプレート側は適当-->
+  <!--  console.logで確認する目的のためテンプレート側は適当-->
   <h1>テスト</h1>
 </template>
 <script setup>
-  // インポートなくても動く
-  import {getHoantyoData, hoge} from "~/composables/testStore";
+// インポートなくても動く
+import {getHoantyoData, hoge} from "~/composables/testStore";
 
-  // 問題なし
-  const getSample = hoge()
-  console.log(getSample)
+// 問題なし
+const getSample = hoge()
+console.log(getSample)
 
-  // API取得問題なし
-  const umiyoho = getHoantyoData()
-  console.log(umiyoho)
+// API取得問題なし
+const umiyoho = getHoantyoData()
+console.log(umiyoho)
 
-  // await付けても問題なく動く
-  const umiyoho2 = await getHoantyoData()
-  console.log(umiyoho2)
+// await付けても問題なく動く
+const umiyoho2 = await getHoantyoData()
+console.log(umiyoho2)
 </script>
